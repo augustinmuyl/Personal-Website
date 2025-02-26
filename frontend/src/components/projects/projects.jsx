963d0f5173ae118448d50ac63602b0734bedbf85
Augustin <augustinusmp@gmail.com>
2025-02-25 21:36:48 -0500

    components added

---
+++ frontend/src/components/projects/projects.jsx
@@ -0,0 +1,14 @@
+import './projects.css';
+
+function Projects() {
+    return(
+        <div id='projects'>
+            <h2>Projects</h2>
+            <div className='project'>
+                <h4>Project #1 </h4>
+                <p>Project #1 Description</p>
+                <a target='_blank' href=""></a>
+            </div>
+        </div>
+    )
+}
