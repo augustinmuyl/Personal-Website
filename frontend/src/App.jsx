306d7efac01aae800b89f0452a0972147a8cd123
Augustin <augustinusmp@gmail.com>
2025-02-25 21:31:48 -0500

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar/navbar'
+import Profile from './components/profile/profile'
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Profile></Profile>
+    </>
+  )
+}
+
+export default App
