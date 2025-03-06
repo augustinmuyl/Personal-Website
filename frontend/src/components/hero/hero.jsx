9dbb7406516647ede57ea9777ef5ef4f1725054a
Augustin <augustinusmp@gmail.com>
2025-03-06 04:18:48 -0500

    fixed hero section + navbar

--- frontend/src/components/hero/hero.jsx
+++ frontend/src/components/hero/hero.jsx
@@ -41,11 +41,15 @@ function Hero() {
             className="w-full h-full flex justify-center lg:justify-start items-center"
         >
             <div className="flex flex-col items-center justify-center text-center lg:items-start lg:ml-56 gap-1">
-                <p className="text-xl lg:text-3xl">Hello, I am</p>
-                <h1 className="font-bold text-3xl lg:text-6xl">
+                <p className="text-lg sm:text-xl md:text-2xl xl:text-3xl">
+                    Hello, I am
+                </p>
+                <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl xl:text-6xl">
                     Augustin Muyl
                 </h1>
-                <p className="text-xl lg:text-3xl">Boston University Student</p>
+                <p className="text-lg sm:text-xl md:text-2xl xl:text-3xl">
+                    Boston University Student
+                </p>
                 <ul className="flex gap-5 mt-4">
                     <a
                         href="https://linkedin.com/in/augustinmuyl"
@@ -55,7 +59,7 @@ function Hero() {
                         <img
                             src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
                             alt="LinkedIn"
-                            className="size-12 hover:scale-125 transition-all duration-300"
+                            className="size-6 sm:size-7 md:size-8 lg:size-10 xl:size-12 hover:scale-125 transition-all duration-300"
                         />
                     </a>
                     <a
@@ -66,7 +70,7 @@ function Hero() {
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                             alt="GitHub"
-                            className="invert size-12 hover:scale-125 transition-all duration-300"
+                            className="invert size-6 sm:size-7 md:size-8 lg:size-10 xl:size-12 hover:scale-125 transition-all duration-300"
                         />
                     </a>
                 </ul>
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import x_icon from "../../../public/assets/x_icon.svg";
+
 function Navbar() {
     const scrollToSection = (sectionId) => {
         const section = document.getElementById(sectionId);
@@ -8,30 +12,52 @@ function Navbar() {
         }
     };
 
+    const buttonRef = useRef(null);
+    const listRef = useRef(null);
+
+    useEffect(() => {
+        const button = buttonRef.current;
+        const list = listRef.current;
+
+        if (!button || !list) return;
+
+        button.addEventListener("click", (e) => {
+            e.stopPropagation();
+            list.classList.toggle("hidden");
+        });
+
+        document.addEventListener("click", () => {
+            list.classList.add("hidden");
+        });
+    }, []);
+
     return (
-        <header className="bg-slate-900 shadow-2xl w-full top-0 left-0 z-50 border-b">
-            <nav className="mx-5  text-violet-300/80">
+        <header className="fixed bg-slate-900 shadow-2xl w-full z-50 border-b p-2 sm:p-3 md:p-4 lg:p-5 xl:p-6">
+            <nav className="mx-5 sm:mx-16 md:mx-30 lg:mx-40 xl:mx-60 flex justify-between items-center text-violet-300/80 font-bold">
                 <a
                     href="/"
-                    className="font-bold text-xl md:text-3xl text-violet-200"
+                    className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-violet-200"
                 >
                     Augustin Muyl
                 </a>
                 <button
                     onClick={() => scrollToSection("about")}
-                    className="hover:text-white"
+                    className="hover:text-white
+                    hidden sm:block text-sm md:text-base lg:text-lg xl:text-xl"
                 >
                     About
                 </button>
                 <button
                     onClick={() => scrollToSection("experience")}
-                    className="hover:text-white"
+                    className="hover:text-white
+                    hidden sm:block text-sm md:text-base lg:text-lg xl:text-xl"
                 >
                     Experience
                 </button>
                 <button
                     onClick={() => scrollToSection("projects")}
-                    className="hover:text-white"
+                    className="hover:text-white
+                    hidden sm:block text-sm md:text-base lg:text-lg xl:text-xl"
                 >
                     Projects
                 </button>
@@ -39,10 +65,83 @@ function Navbar() {
                     href="https://linkedin.com/in/augustinmuyl"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="hover:text-white"
+                    className="hover:text-white
+                    hidden sm:block text-sm md:text-base lg:text-lg xl:text-xl"
                 >
                     Contact
                 </a>
+
+                <button
+                    ref={buttonRef}
+                    className="sm:hidden size-5 mt-[2px] mr-[-10px] hover:text-white"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                        version="1.1"
+                        width="currentWidth"
+                        height="currentHeight"
+                        viewBox="0 0 256 256"
+                        xmlSpace="preserve"
+                    >
+                        <defs></defs>
+                        <g
+                            style={{
+                                stroke: "none",
+                                strokeWidth: 0,
+                                strokeDasharray: "none",
+                                strokeLinecap: "butt",
+                                strokeLinejoin: "miter",
+                                strokeMiterlimit: 10,
+                                fillRule: "nonzero",
+                                opacity: 1,
+                            }}
+                            transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+                        >
+                            <path
+                                d="M 8 90 c -2.047 0 -4.095 -0.781 -5.657 -2.343 c -3.125 -3.125 -3.125 -8.189 0 -11.314 l 74 -74 c 3.125 -3.124 8.189 -3.124 11.314 0 c 3.124 3.124 3.124 8.189 0 11.313 l -74 74 C 12.095 89.219 10.047 90 8 90 z"
+                                fill="currentColor"
+                                strokeLinecap="round"
+                            />
+                            <path
+                                d="M 82 90 c -2.048 0 -4.095 -0.781 -5.657 -2.343 l -74 -74 c -3.125 -3.124 -3.125 -8.189 0 -11.313 c 3.124 -3.124 8.189 -3.124 11.313 0 l 74 74 c 3.124 3.125 3.124 8.189 0 11.314 C 86.095 89.219 84.048 90 82 90 z"
+                                fill="currentColor"
+                                strokeLinecap="round"
+                            />
+                        </g>
+                    </svg>
+                </button>
+                <div
+                    ref={listRef}
+                    className="hidden sm:hidden absolute z-50 bg-slate-900 rounded-2xl p-6 right-4 top-14 text-sm font-semibold flex flex-col items-center gap-2"
+                >
+                    <button
+                        onClick={() => scrollToSection("about")}
+                        className="hover:text-white"
+                    >
+                        About
+                    </button>
+                    <button
+                        onClick={() => scrollToSection("experience")}
+                        className="hover:text-white"
+                    >
+                        Experience
+                    </button>
+                    <button
+                        onClick={() => scrollToSection("projects")}
+                        className="hover:text-white"
+                    >
+                        Projects
+                    </button>
+                    <a
+                        href="https://linkedin.com/in/augustinmuyl"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:text-white"
+                    >
+                        Contact
+                    </a>
+                </div>
             </nav>
         </header>
     );
