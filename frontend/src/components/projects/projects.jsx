a4f3889357477674b584d640920299357ef7b527
Augustin <augustinusmp@gmail.com>
2025-03-18 18:56:18 -0400

    idk

--- frontend/src/components/projects/projects.jsx
+++ frontend/src/components/projects/projects.jsx
@@ -7,8 +7,13 @@ function Projects() {
             <div className='project'>
                 <h4>Project #1 </h4>
                 <p>Project #1 Description</p>
-                <a target='_blank' href=""></a>
+                <a target='_blank' href="https://github.com/augustinmuyl/Outreach-Outlet/"><button className='github'></button></a>
+                <h4>Project #2 </h4>
+                <p>Project #2 Description</p>
+                <a target='_blank' href="https://github.com/augustinmuyl/"><button className='github'></button></a>
             </div>
         </div>
     )
 }
+
+export default Projects;
